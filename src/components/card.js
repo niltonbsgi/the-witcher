520ff5870049deb7eea495ac948a9dc120c14770
nilton <niltonbsgi@gmail.com>
2019-10-01 10:37:48 -0300

    ..

--- src/components/card.js
+++ src/components/card.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 const Card = ({ title, url, onClick}) => {
     return (
-        <div style={style.styleDiv}>
-            <img style={style.styleImage} onClick={ onClick } src={url}/>    
-            <div style={ style.styleDivImage } >
-                <label>{ title }</label>
-            </div>
+        <div style={style.styleDiv}>            
+            <img style={style.styleImage} onClick={ onClick } src={url}/>                
+            <div style={ style.stylefigcaption }>
+                <label style={ style.styleDivLabel } >{ title }</label>
+            </div>           
         </div>
     )
-}
+} 
 Card.propTypes = {
     url: PropTypes.string,
     onClick: PropTypes.func
@@ -30,13 +30,20 @@ const style = {
         marginLeft:'30px',
         marginBottom:'30px',
         border:'solid 1px',
-        borderColor: '#f5e59a'
+        borderColor: '#f5e59a',
+        position:'relative',
     },
-    styleDivImage:{
+    styleDivLabel:{        
         color:'white', 
         textAlign:'center', 
-        width:'100%', 
-        position:'relative' 
+        width:'100%'            
+    },
+    stylefigcaption:{        
+        color:'white', 
+        textAlign:'center', 
+        width:'100%',
+        position:'absolute',
+        bottom:'3px'
     },
     styleImage:{
         cursor: 'pointer',
